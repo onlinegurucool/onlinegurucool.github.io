56512a369dfbb1264532607a81086f5944dd3dda
Pratap Rockerss <pratap@techcetra.com>
2018-10-17 09:13:18 +0530

    home page desktop version fixes and navigation added

--- scripts/common.js
+++ scripts/common.js
@@ -1,19 +1,25 @@
-function showPageLoader(){
-  $('.loader').show();
+function showPageLoader() {
+    $(".loader").show();
 }
-function hidePageLoader(){
-  $('.loader').hide();
+function hidePageLoader() {
+    $(".loader").hide();
 }
-
 
 // This function will check the element is present in DOM then it will exculte callback
-function checkElement(element,cb){
-  if(element.length > 0){
-    cb();
-  }
+function checkElement(element, cb) {
+    if (element.length > 0) {
+        cb();
+    }
 }
 
-$(window).on('load', function(){
-  // hide loader on page load
-  hidePageLoader();
-})
+$(window).on("load", function() {
+    // hide loader on page load
+    $(".toggle-btn,.nav-bg").on("click", function() {
+        if($('.wrapper').hasClass('nav-opened')){
+			$('.wrapper').removeClass('nav-opened')
+		} else {
+			$('.wrapper').addClass('nav-opened')
+		}
+    });
+    hidePageLoader();
+});
